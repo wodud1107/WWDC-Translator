5328ed19e58fad551b9214edcb6fc2d6744f1e50
wodud1107 <wodud7992@naver.com>
2026-03-09 21:44:53 +0900

    [#1] chore: 프로젝트 초기 설정

---
+++ WWDC-Translator/WWDC-Translator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WWDC-Translator
+//
+//  Created by 김재영 on 3/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WWDC-Translator/WWDC-Translator/WWDC_TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC_TranslatorApp.swift
+//  WWDC-Translator
+//
+//  Created by 김재영 on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct WWDC_TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
